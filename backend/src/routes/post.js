5d1700e637065b540664ed2bce71727ca169f15a
vinayak Khandekar <khandekarvinayak41@gmail.com>
2022-01-24 19:09:43 +0530

    like and dislike post api

--- backend/src/routes/post.js
+++ backend/src/routes/post.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { createPost, getPosts, getImage } = require("../controllers/post");
+const { createPost, getPosts, getImage, likePost, dislikePost } = require("../controllers/post");
 const { upload, requireLogin } = require("../utils/middlewares");
 
 router.get("/image/:imageId", requireLogin, getImage)
 router.post("/", requireLogin, upload.array("image"), createPost);
 router.get("/", requireLogin, getPosts);
-
+router.patch("/like/:postId", requireLogin, likePost)
+router.patch("/dislike/:postId", requireLogin, dislikePost)
 module.exports = router;
