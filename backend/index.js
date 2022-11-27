e477a1da40ef0108799f11400c132611320bea4f
vinayak <khandekarvinayak41@gmail.com>
2022-11-27 23:36:27 +0530

    set static files path for server

--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,7 @@ const app = express();
 app.use(cors());
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb', extented: true}));
+app.use(express.static("../frontend/build"))
 
 let gfs;
 mongoose
