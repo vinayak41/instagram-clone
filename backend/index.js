c87a3dfadb05139140ba2464e58e5a695a79b258
vinayak <khandekarvinayak41@gmail.com>
2022-11-28 00:34:33 +0530

    fix: static file path issue

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const mongoose = require("mongoose");
 const config = require("./src/utils/config");
 const { errorHandler, unknownEndpoint } = require("./src/utils/middlewares");
@@ -9,9 +10,9 @@ const postsRouter = require("./src/routes/post");
 const { upload } = require("./src/utils/middlewares");
 const app = express();
 app.use(cors());
-app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb', extented: true}));
-app.use(express.static("../frontend/build"))
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extented: true }));
+app.use(express.static(path.join(__dirname, "../frontend/build")));
 
 let gfs;
 mongoose
